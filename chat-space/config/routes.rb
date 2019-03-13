a5cbcc517daf3ba54520d3236fac2194b52bd8b7
yuya-oki <user@USER-no-MacBook-Air.local>
2019-03-13 12:26:19 +0900

    Edit front

--- chat-space/config/routes.rb
+++ chat-space/config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: { registrations: 'registrations'}
   root 'groups#index'
   resources :users, only: [:index, :edit, :update]
   resources :groups, only: [:new, :create, :edit, :update] do
