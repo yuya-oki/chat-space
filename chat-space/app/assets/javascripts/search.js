cd38be767c827112a8fc3877c43cdaad25697780
yuya-oki <user@USER-no-MacBook-Air.local>
2019-03-28 19:24:28 +0900

    delete console.log

--- chat-space/app/assets/javascripts/search.js
+++ chat-space/app/assets/javascripts/search.js
@@ -24,7 +24,6 @@ $(function () {
   $(function() {
     $(".chat-group-form__input").on("keyup", function() {
       var input = $("#user-search-field").val();
-      console.log
       $.ajax({
         type: 'GET',
         url: '/users',
